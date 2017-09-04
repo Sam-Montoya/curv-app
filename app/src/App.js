c5d5520d38b6f8d78934c9be4941944a88737c80
Brettly <brettly@Brettlys-MacBook-Pro.local>
2017-09-04 16:44:16 -0700

    Animations have been added.

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-import router from './router';
+import {Route, Switch} from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 
@@ -9,19 +9,22 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-
-    }
+    this.state = {}
   }
 
-  render () {
+  render() {
     return (
       <div className='container'>
+        <img className='backgroundImage' 
+        src='https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/dramatic-dark-rainy-storm-clouds-over-gloomy-city-of-los-angeles-skyline-4k-uhd-timelapse_rqswghy__F0004.png'/>
         <section className='logo'>
-          <img className='logo' />
+          <img className='logo' alt=''/>
         </section>
         <div className='userInfo'>
-          {router}
+          <Switch>
+            <Route exact path='/' component={Login}/>
+            <Route path='/signup' component={SignUp}/>
+          </Switch>
         </div>
       </div>
     );
